e5964a7a5119cd7da300b86a99e2a31bd5a0761e
Daniel <danielsilva0.8892@gmail.com>
2020-11-08 01:33:40 +0000

    req body

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.post('/_jsonPost', (req, res) => {
-    res.json("It's Work");
+    res.json(req.body);
 });
 
 app.listen(process.env.PORT || 7000);
