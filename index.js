f97bfcff3774a7ba4d29a9a26511586d854bebbb
Daniel <danielsilva0.8892@gmail.com>
2020-11-08 01:43:24 +0000

    body parser

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const body_parser = require('body_parser');
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/_jsonPost', (req, res) => {
-    console.log(req.body)
-    res.sendStatus(200)
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 7000);
